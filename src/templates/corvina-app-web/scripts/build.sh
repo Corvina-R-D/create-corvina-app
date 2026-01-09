02c93eb8062f37303e83d25fb81419648de854bb
Lorenzo Pistone <lorenpiston@gmail.com>
2026-01-09 16:51:19 +0100

    cleanup unused options and script files

--- src/templates/corvina-app-web/scripts/build.sh
+++ src/templates/corvina-app-web/scripts/build.sh
@@ -17,9 +17,9 @@ TAG=${TAG:-latest}
 REGISTRY=${REGISTRY:-europe-docker.pkg.dev/corvina-app-${app_name}/images}
 
 echo 'Start building app with VITE_SERVICE_URL=' $VITE_SERVICE_URL
-docker build --build-arg VITE_SERVICE_URL -t ${app_name}_app . --progress plain -t ${REGISTRY}/corvina-app-${app_name}-frontend:${TAG}
+docker build --build-arg VITE_SERVICE_URL . --progress plain -t ${REGISTRY}/corvina-app-${app_name}-frontend:${TAG}
 # --------------------------- #
 # build docker image for service
 # --------------------------- #
 cd ../service/
-docker build -t ${app_name}_service . --progress plain -t ${REGISTRY}/corvina-app-${app_name}-backend:${TAG}
+docker build . --progress plain -t ${REGISTRY}/corvina-app-${app_name}-backend:${TAG}
