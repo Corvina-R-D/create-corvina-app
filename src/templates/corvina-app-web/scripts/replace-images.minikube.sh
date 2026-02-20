38cacc7862893bbf4dda1a2321b63b6adf6667d1
Lorenzo Pistone <lorenpiston@gmail.com>
2026-02-20 12:07:05 +0100

    reintroduce --only-backend in replace-images script

--- src/templates/corvina-app-web/scripts/replace-images.minikube.sh
+++ src/templates/corvina-app-web/scripts/replace-images.minikube.sh
@@ -6,11 +6,30 @@ cd $(dirname $0)
 
 . ./common.sh
 
+if [[ $# -eq 1 && "$1" != "--only-backend" ]]; then
+    echo "One argument passed and it's NOT --only-backend: $1"
+    exit
+fi
+
+ONLYBACKEND=false
+if [[ $# -eq 1 && "$1" == "--only-backend" ]]; then
+    ONLYBACKEND=true
+fi
+
 eval $(minikube -p corvina-minikube docker-env)
-REGISTRY=localhost TAG=minikube ./build.sh
+if ! $ONLYBACKEND; then
+    echo "Replacing both frontend and backend images in minikube..."
+    REGISTRY=localhost TAG=minikube ./build.sh
+else
+    echo "Replacing only backend image in minikube..."
+    REGISTRY=localhost TAG=minikube ./build.sh --only-backend
+fi
 eval $(minikube -p corvina-minikube docker-env -u)
 
 kubectl --context corvina-minikube -n corvina-app-${app_name} set image deployment service-deployment service=localhost/corvina-app-${app_name}-backend:minikube
-kubectl --context corvina-minikube -n corvina-app-${app_name} set image deployment app-deployment app=localhost/corvina-app-${app_name}-frontend:minikube
-kubectl --context corvina-minikube -n corvina-app-${app_name} rollout restart deployment service-deployment app-deployment
+kubectl --context corvina-minikube -n corvina-app-${app_name} rollout restart deployment service-deployment
+if ! $ONLYBACKEND; then
+    kubectl --context corvina-minikube -n corvina-app-${app_name} set image deployment app-deployment app=localhost/corvina-app-${app_name}-frontend:minikube
+    kubectl --context corvina-minikube -n corvina-app-${app_name} rollout restart deployment app-deployment
+fi
 kubectl --context corvina-minikube -n corvina-app-${app_name} rollout status deployment service-deployment app-deployment
