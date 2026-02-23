bc68b360c9f227de2707e477923bcf82a501a6b8
Lorenzo Pistone <lorenpiston@gmail.com>
2026-02-23 18:49:13 +0100

    backport --only-backend in build.sh

--- src/templates/corvina-app-web/scripts/build.sh
+++ src/templates/corvina-app-web/scripts/build.sh
@@ -6,18 +6,26 @@ cd $(dirname $0)
 
 . ./common.sh
 
-# --------------------------- #
-# build docker image for app
-# --------------------------- #
-cd ../app/
+if [[ $# -eq 1 && "$1" != "--only-backend" ]]; then
+    echo "One argument passed and it's NOT --only-backend: $1"
+    exit
+fi
 
 # set default value for VITE_SERVICE_URL
-VITE_SERVICE_URL=${VITE_SERVICE_URL:-}
+export VITE_SERVICE_URL=${VITE_SERVICE_URL:-}
 TAG=${TAG:-latest}
 REGISTRY=${REGISTRY:-europe-docker.pkg.dev/corvina-app-${app_name}/images}
 
-echo 'Start building app with VITE_SERVICE_URL=' $VITE_SERVICE_URL
-docker build --build-arg VITE_SERVICE_URL . --progress plain -t ${REGISTRY}/corvina-app-${app_name}-frontend:${TAG}
+if ! [[ $# -eq 1 && "$1" == "--only-backend" ]]; then
+  # --------------------------- #
+  # build docker image for app
+  # --------------------------- #
+  cd ../app/
+
+  echo 'Start building app with VITE_SERVICE_URL=' $VITE_SERVICE_URL
+  docker build --build-arg VITE_SERVICE_URL . --progress plain -t ${REGISTRY}/corvina-app-${app_name}-frontend:${TAG}
+fi
+
 # --------------------------- #
 # build docker image for service
 # --------------------------- #
